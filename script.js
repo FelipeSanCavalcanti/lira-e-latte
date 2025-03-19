e4321a13c4a7660c6b6f61a9116c6801c353a683
FelipeSanCavalcanti <88261152+FelipeSanCavalcanti@users.noreply.github.com>
2025-03-18 21:09:55 -0300

    finalização do carrossel e atualização nos estilos

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 "use strict";
+
+const prevButton = document.getElementById("prev");
+const nextButton = document.getElementById("next");
+const carrosselContainer = document.querySelector(".carrossel-container");
+
+let index = 0;
+const slides = document.querySelectorAll(".carrossel-slide");
+const totalSlides = slides.length;
+console.log(slides);
+console.log(totalSlides);
+
+function updateCarrossel() {
+  const slideWidth = slides[0].clientWidth;
+  carrosselContainer.style.transform = `translateX(${-index * slideWidth}px)`;
+}
+
+nextButton.addEventListener("click", () => {
+  index = (index + 1) % totalSlides;
+  updateCarrossel();
+});
+
+prevButton.addEventListener("click", () => {
+  index = (index - 1 + totalSlides) % totalSlides;
+  updateCarrossel();
+});
+
+setInterval(() => {
+  index = (index + 1) % totalSlides;
+  updateCarrossel();
+}, 5000);
